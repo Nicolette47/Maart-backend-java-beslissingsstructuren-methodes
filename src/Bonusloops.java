571d2af320eda7e3e1a59b400284e05b06928f2b
Nicolette47 <nvanzeijl@gmail.com>
2025-03-17 21:05:06 +0100

    bonus loops stoppen 7,9 en 10 niet

--- src/Bonusloops.java
+++ src/Bonusloops.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Bonusloops {
     public static void main(String[] args) {
@@ -63,10 +65,37 @@ public class Bonusloops {
        int i = 0;
 
        while ( i < opdr5Lijst.length && !opdr5Lijst[i].equals("")) {
-           System.out.print (opdr5Lijst[i] + " , ");
+           System.out.print(opdr5Lijst[i] + " , ");
            i++;
        }
 
+        //OPDRACHT 6
+        // priemgetal is groter dan 1. En alleen deelbaar door 1 en zichzelf.
+
+
+        //OPDRACHT 7
+        int[] opdr7List= {17, 92, 3, 89, 55, 86, 24, 34, 67, 76, 81, 50, 59, 30, 14, 33, 83, 87, 38, 70, 9, 78, 91, 46, 53, 64, 25, 69, 29, 84, 22, 16, 79, 40, 21, 18, 68, 75, 2, 26, 39, 8, 31, 35, 1, 63, 77, 95, 15, 58, 61, 10, 85, 93, 96, 72, 98, 5, 73, 6, 23, 56, 49, 99, 43, 41, 20, 11, 12, 74, 57, 66, 94, 36, 7, 60, 88, 4, 65, 27, 28, 82, 32, 97, 48, 62, 80, 37, 13, 54, 19, 71, 42, 44, 90, 52, 51};
+
+
+        List<Integer> lijst45YMas = new ArrayList<>();
+
+       for (i = 0; i < opdr7List.length; i++) {
+           if (opdr7List[i] > 45 ){
+               lijst45YMas.add(opdr7List[i]);
+           }
+       }
+        System.out.println("lijst45YMas = " + lijst45YMas);
+
+
+       //OPDRACHT 8
+
+        ArrayList<Integer>opdracht8 = new ArrayList<>();
+        for ( i = 10; i>= 0; i-- ){
+            opdracht8.add(i);
+        }
+        System.out.println("aftel array: " + opdracht8);
 
     }
+
+
 }
